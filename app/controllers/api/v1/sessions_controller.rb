3b91683e29966ed782f76503f1d3226fd3161b74
ashawareb <bzsc1010@gmail.com>
2022-03-27 21:54:39 +0200

    apply rubocop guides and creating a basic login action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,25 @@
-class Api::V1::SessionsController < Api::V1::BaseController
-  skip_before_action :verify_authenticity_token
+module Api
+  module V1
+    # This controller is to manage our application sessions
+    class SessionsController < BaseController
+      skip_before_action :verify_authenticity_token
 
-  def create
-    render json: { welcome_message: 'Hello motherfucker!' }, status: :ok
+      def create
+        user = User.find_by(email: params[:email])
+        if user&.valid_password?(params[:password])
+          render json: { message: 'Loged in successfully' }, status: :ok
+        else
+          render json: { message: 'Wrong email or password' }, status: :unauthorized
+        end
+      end
+
+      def destroy; end
+
+      private
+
+      def user_parasm
+        params.require(:user).permit(:email, :password)
+      end
+    end
   end
-
-  def destroy; end
 end

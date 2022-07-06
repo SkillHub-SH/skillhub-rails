219129c506c5d417dbc8356d35a8e4eb52230ff6
ashawareb <bzsc1010@gmail.com>
2022-07-07 00:33:27 +0200

    add topic to problem serializer

--- app/serializers/api/v1/problem_serializer.rb
+++ app/serializers/api/v1/problem_serializer.rb
@@ -5,6 +5,7 @@ module Api
       attributes :id, :title, :body, :difficullty, :memory_limit, :time_limit,
                  :input, :output, :input_description, :output_description, :notes,
                  :accepted_percentage, :created_at, :updated_at, :company_id, :contest_id
+      belongs_to :topic
 
       def accepted_percentage
         object.accepted_percentage

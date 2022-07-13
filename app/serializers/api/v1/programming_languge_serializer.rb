39b7b7dd3c02c5bd13d1211fa0ca3b763dbc257a
ashawareb <bzsc1010@gmail.com>
2022-07-13 14:34:27 +0200

    add leaderboard and add extension to programming languages

--- app/serializers/api/v1/programming_languge_serializer.rb
+++ app/serializers/api/v1/programming_languge_serializer.rb
@@ -2,11 +2,7 @@ module Api
   module V1
     # Programming Language serializer to return the following attributes
     class ProgrammingLangugeSerializer < ActiveModel::Serializer
-      attributes :id, :name, :judge_code, :default_languages_names
-
-      def default_languages_names
-        ProgrammingLanguge.default_languages_names
-      end
+      attributes :id, :name, :judge_code, :extension
     end
   end
 end
